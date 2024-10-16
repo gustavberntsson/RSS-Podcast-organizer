1ade5e57037c57e81639391be2aa82e571109677
Gustav Berntsson <121899011+gustavberntsson@users.noreply.github.com>
2024-10-16 14:20:21 +0200

    tagit bort onödiga eventlisteners

--- PoddApp/Form1.cs
+++ PoddApp/Form1.cs
@@ -7,39 +7,5 @@ namespace PoddApp
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Avsnitt_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
